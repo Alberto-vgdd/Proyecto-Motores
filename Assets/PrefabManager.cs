8befdd7a1ed816dd6d0bea2880ae45f256359f1f
Alberto-vgdd <alberto.vgdd@gmail.com>
2017-04-26 15:13:40 +0200

    A tope con la coepe

---
+++ Assets/PrefabManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabManager : MonoBehaviour {
+
+	public static PrefabManager currentInstance;
+
+	public GameObject[] envDecoCity;
+
+	void Awake()
+	{
+		currentInstance = this;
+	}
+	public GameObject GetRandomDeco(string biome)
+	{
+		return envDecoCity [Random.Range (0, envDecoCity.Length)];
+	}
+}
