3ab809dcce14c8c1e817dae661c237dd0c719e7f
al286368 <al286368@uji.es>
2017-06-22 12:39:11 +0200

    Iluminacion
    - Cambio de color de la luz dependiendo de la hora.
    - Arreglada una incompatibilidad con la version.

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -20,10 +20,18 @@ public class DayNightCycle : MonoBehaviour
 
 	private float rotationOffset = 250f;
 	private bool lightsOn = false;
-	private float lightsOnStart = 19f;
+	private float lightsOnStart = 19.5f;
 	private float lightsOnEnd = 7f;
 
 	private Light directionalLight;
+
+	public Color illumMidday;
+	public Color illumMidnight;
+	public Color illumSunrise;
+	public Color illumSunset;
+
+	private Color currentIllumColor;
+	private float currentIllumT;
 
 	// Use this for initialization
 	void Awake ()
@@ -32,6 +40,7 @@ public class DayNightCycle : MonoBehaviour
 	}
 	void Start () 
     {
+		//RenderSettings.skybox = m_Skyboxes [1];
 		directionalLight = GetComponent<Light> ();
 	}
 	
@@ -43,6 +52,7 @@ public class DayNightCycle : MonoBehaviour
 		if (currentHour > 24)
 			currentHour = 0;
 		transform.rotation = Quaternion.Euler (((currentHour/24) * 360) + rotationOffset,0,0);
+		UpdateIllumColor ();
 
 		if (currentHour > 12) { directionalLight.intensity = 1 - (currentHour-12)/12f; }
 		else { directionalLight.intensity = currentHour / 12f; }
@@ -63,6 +73,27 @@ public class DayNightCycle : MonoBehaviour
 			}
 		}
 
+	}
+	void UpdateIllumColor()
+	{
+		if (currentHour < 5) {
+			currentIllumT = currentHour / 5f;
+			currentIllumColor = Color.Lerp (illumMidnight, illumSunrise, currentIllumT);
+		} else if (currentHour < 10) {
+			currentIllumT = (currentHour-5) / 5f;
+			currentIllumColor = Color.Lerp (illumSunrise, illumMidday, currentIllumT);
+		} else if (currentHour < 19) {
+			currentIllumT = (currentHour-10) / 9f;
+			currentIllumColor = Color.Lerp (illumMidday, illumSunset, currentIllumT);
+		} else if (currentHour < 21) {
+			currentIllumT = (currentHour-19) / 2f;
+			currentIllumColor = Color.Lerp (illumSunset, illumMidnight, currentIllumT);
+		} else{
+			currentIllumT = (currentHour-21) / 3f;
+			currentIllumColor = Color.Lerp (illumMidnight, illumMidnight, currentIllumT);
+		}
+		directionalLight.color = currentIllumColor;
+			
 	}
 	public void SetTimeAndTimescale(float time, float timescale)
 	{
