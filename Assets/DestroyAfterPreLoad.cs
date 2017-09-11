4e1d2351897749920e6bf7dceec1b2fca2e1ee06
al286368 <al286368@uji.es>
2017-09-12 00:11:35 +0200

    PreLoad
    - Añadida una precarga de todos los modelados y luces.
    - E
    - Z

---
+++ Assets/DestroyAfterPreLoad.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterPreLoad : MonoBehaviour {
+
+	void Start () {
+		Destroy(this.gameObject, (0.1f));
+	}
+
+}
