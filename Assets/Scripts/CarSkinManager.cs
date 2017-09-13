31e3bc748c26faf184f2631a556fb9af4ce54ad7
al286368 <al286368@uji.es>
2017-09-13 22:08:34 +0200

    Ahora si, cambio de pintura terminado.

--- Assets/Scripts/CarSkinManager.cs
+++ Assets/Scripts/CarSkinManager.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class CarSkinManager : MonoBehaviour {
 
-	public List<MeshRenderer> MRs;
+	public List<MeshRenderer> MeshRenderers;
+	public MeshRenderer specialCase1;
+	public MeshRenderer specialCase2;
 
 	public void ChangeBaseSkin(int id)
 	{
-		for (int i = 0; i < MRs.Count; i++) {
-			MRs[i].material = GlobalGameData.currentInstance.m_carSkins [id];
+		for (int i = 0; i < MeshRenderers.Count; i++) {
+			MeshRenderers[i].material = GlobalGameData.currentInstance.m_carSkins [id];
 		}
+
+		// TODO: me duele escribir esto pero unity no quiere otra solucion.
+		if (specialCase1 != null) {
+			Material[] tempMat = specialCase1.materials;
+			tempMat [1] = GlobalGameData.currentInstance.m_carSkins [id];
+			specialCase1.materials = tempMat;
+		}
+		if (specialCase2 != null) {
+			Material[] tempMat = specialCase2.materials;
+			tempMat [1] = GlobalGameData.currentInstance.m_carSkins [id];
+			specialCase2.materials = tempMat;
+		}
+
 	}
 
 }
